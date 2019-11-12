60a4e1684af53f31d646702719b199673f671cec
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-11-12 15:45:58 +0100

    Card flip and basic styling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const cardsContainer = document.querySelector(".cards-container");
+
 // Fetch Data from API
 
 function getData() {
@@ -5,14 +7,33 @@ function getData() {
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      console.log(data);
-      data.books.forEach(book => console.log(book.cover))
+      data.books.forEach(book => {
+        console.log(book);
+
+        let card = document.createElement("div");
+        card.className = "flip-card";
+        card.innerHTML = `
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src="${book.cover}" alt="Avatar" style="width:300px;height:400px;">
+            </div>
+            <div class="flip-card-back">
+              <h1>${book.title}</h1> 
+              <p>${book.description}</p> 
+              <p>${book.language}</p>
+              <p>${book.detail}</p>
+            </div>
+          </div>`
+        cardsContainer.appendChild(card);
+
+        // let el = document.createElement("div");
+        // el.className = "book";
+        // el.style.backgroundImage = `url(${book.cover})`
+        // booksContainer.appendChild(el)
+      })
     })
 }
 getData()
-
-
-
 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.addEventListener("scroll", scrollFunction);
