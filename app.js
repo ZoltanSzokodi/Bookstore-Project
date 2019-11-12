4e5cfa5daa39156db19e41284fc9003c43cd3d80
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-11-12 19:45:28 +0100

    Navbar responsive, prototype searchbar implemented

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+window.addEventListener("scroll", scrollFunction);
+const navbar = document.querySelector(".navbar");
+const navbarLogo = document.querySelector(".navbar-elements_logo");
+const navbarLinks = document.querySelectorAll(".nav-l");
 const cardsContainer = document.querySelector(".cards-container");
 
 // Fetch Data from API
@@ -15,7 +19,7 @@ function getData() {
         card.innerHTML = `
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img src="${book.cover}" alt="Avatar" style="width:300px;height:400px;">
+              <img src="${book.cover}" alt="Avatar">
             </div>
             <div class="flip-card-back">
               <h1>${book.title}</h1> 
@@ -25,35 +29,26 @@ function getData() {
             </div>
           </div>`
         cardsContainer.appendChild(card);
-
-        // let el = document.createElement("div");
-        // el.className = "book";
-        // el.style.backgroundImage = `url(${book.cover})`
-        // booksContainer.appendChild(el)
       })
     })
 }
 getData()
 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.addEventListener("scroll", scrollFunction);
-const navbar = document.querySelector(".navbar");
-const logoContainer = document.querySelector(".logo-container");
-const navbarLinks = document.querySelectorAll(".nav-l");
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     navbar.style.padding = "0rem";
     navbar.style.backgroundColor = "white";
-    logoContainer.style.color = "#261431";
-    logoContainer.style.fontSize = "2rem";
-    navbarLinks.forEach(l => l.style.color = "#261431");
+    navbarLogo.style.color = "#261431";
+    navbarLogo.style.fontSize = "2rem";
+    navbarLinks.forEach(link => link.style.color = "#261431");
   } else {
-    navbar.style.padding = "8rem 1rem";
+    navbar.style.padding = "6rem 1rem 8rem";
     navbar.style.backgroundColor = "transparent";
-    logoContainer.style.color = "white";
-    logoContainer.style.fontSize = "3rem";
-    navbarLinks.forEach(l => l.style.color = "white");
+    navbarLogo.style.color = "white";
+    navbarLogo.style.fontSize = "3rem";
+    navbarLinks.forEach(link => link.style.color = "white");
   }
 }
 
